57e601b022e03bfc40efdb4a0ab415e04b344f61
Spencer Smith <dontaskspencer@gmail.com>
2024-02-10 23:50:29 -0700

    Add reconnect

--- src/server.js
+++ src/server.js
@@ -2,9 +2,61 @@ require('dotenv').config();
 
 const mqtt = require('mqtt');
 const express = require('express');
-const { WebSocketServer, WebSocket } = require('ws');
+const { WebSocket } = require('ws');
 
 const { WS_PORT, MQTT_SERVER, EXPRESS_PORT } = process.env;
+
+/**
+ * Set up ws server
+ */
+const wss = new WebSocket.Server({
+  port: WS_PORT,
+  clientTracking: true,
+});
+wss.on('connection', handleConnection);
+
+wss.on('listening', () => {
+  console.log(`Websocket server listening at ws://localhost:${wss.address().port}`);
+});
+
+wss.on('error', (error) => {
+  console.log('websocket error:', error);
+});
+
+/**
+ * Returns a list of approved websocket clients
+ */
+function getClients() {
+  return Array.from(wss.clients);
+}
+
+/**
+ * Handles new websocket connections
+ */
+function handleConnection(ws) {
+  console.log('✔ connected to websocket client');
+  ws.isAlive = true;
+
+  // response from heartbeat
+  ws.on('pong', () => {
+    ws.isAlive = true;
+  });
+
+  ws.on('close', (code) => {
+    console.log(`  closed with code: ${code}`);
+  });
+}
+
+/**
+ * Terminates any nonresponsive clients
+ */
+setInterval(() => {
+  wss.clients.forEach((ws) => {
+    if (ws.isAlive === false) return ws.terminate();
+    ws.isAlive = false;
+    ws.ping(() => {});
+  });
+}, 5000);
 
 /**
  * Connect and subscribe to mqtt
@@ -17,16 +69,6 @@ client.on('connect', () => {
     }
   });
 });
-
-/**
- * Set up ws server
- */
-const wss = new WebSocketServer({ port: WS_PORT });
-wss.on('connection', function connection(ws) {
-  ws.on('error', console.error);
-});
-
-const latestTopics = {};
 
 /**
  * Forward message to all ws clients
@@ -44,9 +86,15 @@ client.on('message', (topic, messageBuffer) => {
   });
 });
 
+/**
+ * Add express server for debug info
+ */
 const expressApp = express();
 expressApp.set('json spaces', 2);
 expressApp.listen(EXPRESS_PORT);
-expressApp.get('/latest', (req, res) => {
-  res.json(latestTopics);
+expressApp.get('/info', (req, res) => {
+  const list = Array.from(wss.clients);
+  res.json({
+    numClients: list.length,
+  });
 });
